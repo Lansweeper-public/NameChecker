89a873bc7ccf4431905843ef6b4a1106ac2a5e97
Rafa Perez <fetilla@gmail.com>
2022-02-28 09:53:53 +0100

    :recycle: remove semantic release dependency

--- release.config.js
+++ release.config.js
@@ -1,10 +1,53 @@
-const { createReleaseConfig } = require("@lansweeper/semantic-release-config");
-const { version: previousVersion } = require("./package.json");
+const major = [":tada:", ":boom:"];
+const minor = [":sparkles:"];
+const patch = [
+  ":bug:",
+  ":ambulance:",
+  ":art:",
+  ":recycle:",
+  ":fire:",
+  ":construction_worker:",
+  ":rocket:",
+  ":arrow_up:",
+  ":wheel_of_dharma:",
+  ":zap:",
+  ":arrow_up:",
+];
 
-module.exports = createReleaseConfig({
-  commitAssets: ["sonar-project.properties"],
-  execOptions: {
-    prepareCmd: `sed -i "s|${previousVersion}|\${nextRelease.version}|g" sonar-project.properties`,
-    publishCmd: "touch .released",
-  },
-});
+module.exports = {
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "atom",
+        releaseRules: [
+          ...major.map((emoji) => ({ emoji, release: "major" })),
+          ...minor.map((emoji) => ({ emoji, release: "minor" })),
+          ...patch.map((emoji) => ({ emoji, release: "patch" })),
+        ],
+      },
+    ],
+    "@semantic-release/github",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "atom",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          ":bookmark: Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
